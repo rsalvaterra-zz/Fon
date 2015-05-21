5c4282f35690bb94666fdb83bbf59173ae8a4242
Rui Salvaterra <rsalvaterra@gmail.com>
2015-05-21 09:21:44 +0100

    Minor simplification.

--- src/org/rsalvaterra/fon/WifiBroadcastReceiver.java
+++ src/org/rsalvaterra/fon/WifiBroadcastReceiver.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.net.NetworkInfo.State;
 import android.net.wifi.WifiManager;
 
 public final class WifiBroadcastReceiver extends BroadcastReceiver {
@@ -17,9 +18,10 @@ public final class WifiBroadcastReceiver extends BroadcastReceiver {
 		} else if (a.equals(WifiManager.NETWORK_STATE_CHANGED_ACTION)) {
 			final NetworkInfo ni = (NetworkInfo) i.getParcelableExtra(WifiManager.EXTRA_NETWORK_INFO);
 			if ((ni.getType() == ConnectivityManager.TYPE_WIFI)) {
-				if (ni.getState() == NetworkInfo.State.CONNECTED) {
+				final State s = ni.getState();
+				if (s == State.CONNECTED) {
 					ActionExecutor.execute(c, new Intent(c, ActionExecutor.class).setAction(Constants.ACT_LOGIN).putExtra(Constants.KEY_LOGIN, true));
-				} else if (ni.getState() == NetworkInfo.State.DISCONNECTED) {
+				} else if (s == State.DISCONNECTED) {
 					ActionExecutor.execute(c, new Intent(c, ActionExecutor.class).setAction(Constants.ACT_CANCEL_ALL));
 				}
 			}
