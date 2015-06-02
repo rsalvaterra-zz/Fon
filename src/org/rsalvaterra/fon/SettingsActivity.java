96110dea70363c86b90d58506951990f58157f04
Rui Salvaterra <rsalvaterra@gmail.com>
2015-06-02 17:01:25 +0100

    Have the preference activity implement the required interfaces instead of using anonymous classes.

--- src/org/rsalvaterra/fon/SettingsActivity.java
+++ src/org/rsalvaterra/fon/SettingsActivity.java
@@ -15,46 +15,7 @@ import android.text.Html;
 import android.text.method.LinkMovementMethod;
 import android.widget.TextView;
 
-public final class SettingsActivity extends PreferenceActivity {
-
-	private static final OnPreferenceClickListener OC_LISTENER = new OnPreferenceClickListener() {
-
-		@Override
-		public boolean onPreferenceClick(final Preference p) {
-			final Context c = p.getContext();
-			((TextView) new Builder(c).setIcon(R.drawable.ic_launcher).setTitle(R.string.app_name).setMessage(Html.fromHtml(c.getString(R.string.app_credits, c.getString(R.string.app_copyright), c.getString(R.string.app_source)))).setNeutralButton(R.string.accept, new OnClickListener() {
-
-				@Override
-				public void onClick(final DialogInterface dialog, final int which) {
-					dialog.dismiss();
-
-				}
-			}).show().findViewById(android.R.id.message)).setMovementMethod(LinkMovementMethod.getInstance());
-			return true;
-		}
-	};
-
-	private static final OnPreferenceChangeListener OPC_LISTENER = new OnPreferenceChangeListener() {
-
-		@Override
-		public final boolean onPreferenceChange(final Preference p, final Object v) {
-			final Context c = p.getContext();
-			final String k = p.getKey();
-			String s;
-			if (k.equals(c.getString(R.string.kperiod))) {
-				s = c.getString(R.string.periodSummary, v);
-			} else if (k.equals(c.getString(R.string.krssi))) {
-				s = c.getString(R.string.rssiSummary, v);
-			} else {
-				s = (String) v;
-				if (s.length() != 0) {
-					s = RingtoneManager.getRingtone(c, Uri.parse(s)).getTitle(c);
-				}
-			}
-			p.setSummary(s);
-			return true;
-		}
-	};
+public final class SettingsActivity extends PreferenceActivity implements OnClickListener, OnPreferenceChangeListener, OnPreferenceClickListener {
 
 	private Preference getPreference(final int id) {
 		return getPreferenceScreen().findPreference(getString(id));
@@ -62,12 +23,17 @@ public final class SettingsActivity extends PreferenceActivity {
 
 	private void setOnPreferenceChangeListener(final int id, final String v) {
 		final Preference p = getPreference(id);
-		p.setOnPreferenceChangeListener(SettingsActivity.OPC_LISTENER);
-		SettingsActivity.OPC_LISTENER.onPreferenceChange(p, WakefulService.getPreference(p.getContext(), id, v));
+		p.setOnPreferenceChangeListener(this);
+		onPreferenceChange(p, WakefulService.getPreference(p.getContext(), id, v));
 	}
 
 	private void setOnPreferenceClickListener(final int id) {
-		getPreference(id).setOnPreferenceClickListener(SettingsActivity.OC_LISTENER);
+		getPreference(id).setOnPreferenceClickListener(this);
+	}
+
+	@Override
+	public void onClick(final DialogInterface d, final int w) {
+		d.dismiss();
 	}
 
 	@Override
@@ -80,4 +46,31 @@ public final class SettingsActivity extends PreferenceActivity {
 		setOnPreferenceChangeListener(R.string.kfailure, "");
 		setOnPreferenceClickListener(R.string.kabout);
 	}
+
+	@Override
+	public boolean onPreferenceChange(final Preference p, final Object v) {
+		final Context c = p.getContext();
+		final String k = p.getKey();
+		String s;
+		if (k.equals(c.getString(R.string.kperiod))) {
+			s = c.getString(R.string.periodSummary, v);
+		} else if (k.equals(c.getString(R.string.krssi))) {
+			s = c.getString(R.string.rssiSummary, v);
+		} else {
+			s = (String) v;
+			if (s.length() != 0) {
+				s = RingtoneManager.getRingtone(c, Uri.parse(s)).getTitle(c);
+			}
+		}
+		p.setSummary(s);
+		return true;
+	}
+
+	@Override
+	public boolean onPreferenceClick(final Preference p) {
+		final Context c = p.getContext();
+		((TextView) new Builder(c).setIcon(R.drawable.ic_launcher).setTitle(R.string.app_name).setMessage(Html.fromHtml(c.getString(R.string.app_credits, c.getString(R.string.app_copyright), c.getString(R.string.app_source)))).setNeutralButton(R.string.accept, this).show().findViewById(android.R.id.message)).setMovementMethod(LinkMovementMethod.getInstance());
+		return true;
+	}
+
 }
