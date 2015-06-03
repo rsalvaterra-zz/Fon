96afab3a67c7f0f7779eff2285576af246418e42
Rui Salvaterra <rsalvaterra@gmail.com>
2015-06-03 15:33:31 +0100

    Let's try this again.

--- src/org/rsalvaterra/fon/HttpClient.java
+++ src/org/rsalvaterra/fon/HttpClient.java
@@ -1,39 +1,26 @@
 package org.rsalvaterra.fon;
 
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
-import java.util.zip.GZIPInputStream;
 
-import org.apache.http.Header;
-import org.apache.http.HeaderElement;
-import org.apache.http.HttpException;
-import org.apache.http.HttpRequest;
-import org.apache.http.HttpRequestInterceptor;
-import org.apache.http.HttpResponse;
-import org.apache.http.HttpResponseInterceptor;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.methods.HttpUriRequest;
 import org.apache.http.client.params.ClientPNames;
-import org.apache.http.entity.HttpEntityWrapper;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.params.BasicHttpParams;
 import org.apache.http.params.HttpConnectionParams;
 import org.apache.http.params.HttpParams;
 import org.apache.http.protocol.BasicHttpContext;
-import org.apache.http.protocol.HttpContext;
 import org.apache.http.util.EntityUtils;
 
-final class HttpClient extends DefaultHttpClient implements HttpRequestInterceptor, HttpResponseInterceptor {
+final class HttpClient extends DefaultHttpClient {
 
 	private static final int HTTP_TIMEOUT = 30 * 1000;
 
-	private static final String ENCODING_GZIP = "gzip";
-	private static final String HEADER_ACCEPT_ENCODING = "Accept-Encoding";
 	private static final String TAG_WISPR_PASSWORD = "Password";
 	private static final String TAG_WISPR_USERNAME = "UserName";
 	private static final String USER_AGENT = "User-Agent";
@@ -42,8 +29,6 @@ final class HttpClient extends DefaultHttpClient implements HttpRequestIntercept
 
 	{
 		setCookieStore(null);
-		addRequestInterceptor(this);
-		addResponseInterceptor(this);
 		final HttpParams p = new BasicHttpParams().setParameter(HttpClient.USER_AGENT, HttpClient.USER_AGENT_STRING).setBooleanParameter(ClientPNames.ALLOW_CIRCULAR_REDIRECTS, true);
 		HttpConnectionParams.setConnectionTimeout(p, HttpClient.HTTP_TIMEOUT);
 		HttpConnectionParams.setSoTimeout(p, HttpClient.HTTP_TIMEOUT);
@@ -77,36 +62,4 @@ final class HttpClient extends DefaultHttpClient implements HttpRequestIntercept
 		return request(r);
 	}
 
-	@Override
-	public void process(final HttpRequest r, final HttpContext c) throws HttpException, IOException {
-		if (!r.containsHeader(HttpClient.HEADER_ACCEPT_ENCODING)) {
-			r.addHeader(HttpClient.HEADER_ACCEPT_ENCODING, HttpClient.ENCODING_GZIP);
-		}
-	}
-
-	@Override
-	public void process(final HttpResponse r, final HttpContext c) throws HttpException, IOException {
-		// Inflate any responses compressed with gzip
-		final Header encoding = r.getEntity().getContentEncoding();
-		if (encoding != null) {
-			final HeaderElement[] elements = encoding.getElements();
-			for (final HeaderElement element : elements) {
-				if (element.getName().equalsIgnoreCase(HttpClient.ENCODING_GZIP)) {
-					r.setEntity(new HttpEntityWrapper(r.getEntity()) {
-
-						@Override
-						public InputStream getContent() throws IOException {
-							return new GZIPInputStream(wrappedEntity.getContent());
-						}
-
-						@Override
-						public long getContentLength() {
-							return -1;
-						}
-					});
-					break;
-				}
-			}
-		}
-	}
 }
